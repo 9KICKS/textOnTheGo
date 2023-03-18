3511d3a1df3be270f8a8212c480a97927d7b6906
9KICKS <officialjoshua9@gmail.com>
2023-03-18 23:40:26 +0100

    "updated TextOnTheGo.java"

--- src/controllers/TextController.java
+++ src/controllers/TextController.java
@@ -1,12 +1,32 @@
 package controllers;
 
+import data.models.Text;
+import data.repositories.TextRepository;
+import dtos.requests.ReceiveMessageRequest;
 import dtos.requests.SendMessageRequest;
+import dtos.responses.ReceiveMessageResponse;
 import dtos.responses.SendMessageResponse;
+import java.util.List;
 
 public class TextController {
+    private final TextRepository textRepository;
+
+    public TextController(TextRepository textRepository) {
+        this.textRepository = textRepository;
+    }
+
     public SendMessageResponse sendMessage(SendMessageRequest request) {
-        String status = "Success.";
-        String message = "Message sent successfully.";
-        return new SendMessageResponse(status, message);
+        Text text = new Text(request.getSender(), request.getRecipient(), request.getMessage());
+        textRepository.addText(text);
+        return new SendMessageResponse("success", "Message sent successfully.");
+    }
+
+    public ReceiveMessageResponse receiveMessages(ReceiveMessageRequest request) {
+        List<Text> texts = textRepository.getTextsByRecipient(request.getRecipient());
+        if (texts.isEmpty()) {
+            return new ReceiveMessageResponse(null, "No messages found.");
+        } else {
+            return new ReceiveMessageResponse(texts, "Messages retrieved successfully.");
+        }
     }
 }
